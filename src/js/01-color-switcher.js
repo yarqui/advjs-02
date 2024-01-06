75816972143a092c251730bfc1ac3c43825d159b
yarqui <y.pelykh@gmail.com>
2024-01-06 13:00:33 -0500

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,43 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+const ATTRIBUTES = {
+  disabled: 'disabled',
+};
+
+let intervalId;
+
+const getRandomHexColor = () => {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+};
+
+const toggleAttribute = (attr, elements) => {
+  elements.forEach(element => element.toggleAttribute(attr));
+};
+
+const handleStart = () => {
+  toggleAttribute(ATTRIBUTES.disabled, [refs.startBtn, refs.stopBtn]);
+
+  intervalId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  console.log('Active interval id:', intervalId);
+};
+
+const handleStop = () => {
+  clearInterval(intervalId);
+  toggleAttribute(ATTRIBUTES.disabled, [refs.startBtn, refs.stopBtn]);
+
+  console.log('Clear active interval id:', intervalId);
+
+  intervalId = null;
+};
+
+refs.startBtn.addEventListener('click', handleStart);
+refs.stopBtn.addEventListener('click', handleStop);
